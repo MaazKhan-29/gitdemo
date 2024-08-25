c9587b25f8847f3f30877bfd9cdc887ef845a170
Maaz Khan <maazk8448@gmail.com>
2024-08-26 01:14:40 +0530

    update

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,10 +1,11 @@
 let menu = document.querySelector(".menubar");
 let icon = document.querySelector(".fa-solid");
 let icon_div = document.querySelector(".hamburger");
-
+let body = document.querySelector(".body");
 function menubar_open() {
     menu.style.display = "block";
     icon.classList.replace("fa-bars", "fa-xmark");
+    body.style.overflow="hidden";
     if (icon_div.className.includes("hamburger")) {
         icon_div.classList.replace("hamburger", "back")
         icon_div.setAttribute('onClick',"menubar_close()");
@@ -12,6 +13,7 @@ function menubar_open() {
 }
 function menubar_close() {
     menu.style.display = "none";
+    body.style.overflow="auto";
     icon.classList.replace("fa-xmark", "fa-bars");
     if (icon_div.className.includes("back")) {
         icon_div.classList.replace("back","hamburger")
